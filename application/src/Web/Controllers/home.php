9f31da2819e9015ae894707e39dcc129e53439d9
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-19 11:39:44 +0200

    Add post data and check validation

--- application/src/Web/Controllers/home.php
+++ application/src/Web/Controllers/home.php
@@ -5,16 +5,37 @@
 
 namespace Web\Controllers;
 
-use Fonto\Core\Controller;
+use Fonto\Core\Controller,
+	Web\Models\Post;
 
 class Home extends Controller
 {
 	public function indexAction()
 	{
+		$posts = new Post();
+
 		$data = array(
-			'title' => 'Fonto PHP Framework',
-			'text'  => 'Under development!'
+			'title' => 'En gästbok..',
+			'posts' => $posts->getAll()
 		);
+
+		if ($_POST) {
+			$insert = array(
+				'title' => $_POST['title'],
+				'user'  => $_POST['user'],
+				'post'  => $_POST['post'],
+			);
+
+			$validate = $posts->validation($insert);
+
+			if ($validate === true) {
+				// $data['success'] = 'Ditt inlägg är skapat!';
+				$posts->insert($insert);
+				redirect('/');
+			} else {
+				$data['error'] = $validate;
+			}
+		}
 
 		return $this->view('home/index', $data);
 	}
