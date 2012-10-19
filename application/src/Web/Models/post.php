6b1fc95ce9a04f02b17a9435e74bc09776cdf0e1
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-19 09:58:45 +0200

    Add simple validation rules

--- application/src/Web/Models/post.php
+++ application/src/Web/Models/post.php
@@ -7,10 +7,38 @@ use Fonto\Core\Model;
 class Post extends Model
 {
 
+	public function validation($user = '', $title = '', $post = '')
+	{
+		$error = array();
+		$valid = false;
+
+		if ($user == '' or strlen($user) < 2) {
+			$error['user'] = 'Du heter förmodligen något annat.';
+		}
+
+		if ($title == '' or strlen($title) < 5) {
+			$error['title'] = 'Titeln måste vara minst 5 bokstäver.';
+		}
+
+		if ($post == '' or strlen($post) < 7) {
+			$error['post'] = 'Meddelandet måste vara minst 7 bokstäver långt.';
+		}
+
+		if (empty($error)) {
+			return true;
+		}
+
+		return $error;
+	}
+
+
 	public function getAll()
 	{
 		return Post::find('all');
 	}
 
-
+	public function insert($data = array())
+	{
+		Post::create($data);
+	}
 }
